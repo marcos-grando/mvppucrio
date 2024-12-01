dee9079f7b4e610639a255068af3ba665befb828
MarcosAGrando <marcosgrando.03@gmail.com>
2024-12-01 14:20:13 -0300

    commitei

--- src/Index.jsx
+++ src/Index.jsx
@@ -24,7 +24,7 @@ import '../estilo/estilo-singlenews/singlenews.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/mvppucrio' : '/'}>
             <App />
         </BrowserRouter>
     </React.StrictMode>
